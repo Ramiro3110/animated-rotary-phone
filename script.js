7f99b468bd2a3a2e81871bf021dae776f3e233ce
Ramiro3110 <rcalletano@willamette.edu>
2025-07-10 16:59:55 +0000

    fixxy fix

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-/* A function is a named action. */
-function hello() {
-
-    /* An alert is a text pop-up */
-    alert("Hello, world!")
-    
-} /* Enclose the action in {} */
-
 function hello() {
     alert("Hello, world!")
 }
