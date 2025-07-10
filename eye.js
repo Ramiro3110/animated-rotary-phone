8846651b5b3d8e67493490892ab805df4d1e9be2
Ramiro3110 <rcalletano@willamette.edu>
2025-07-10 17:20:36 +0000

    step we; fngpasrjn grhedc

---
+++ eye.js
@@ -0,0 +1,13 @@
+function hello(event) {
+    /* Find the element by id */
+    const element = document.getElementById("pupil");
+    /* Find the new "x" value*/
+    const x = 3 * event.pageX / window.innerWidth;
+    /* Find the new "y" value*/
+    const y = 3 * event.pageY / window.innerHeight;
+    /* Update */
+    element.style["left"] = x + "vmin";
+    element.style["top"] = y + "vmin";
+}
+
+document.addEventListener('click', hello(event));
